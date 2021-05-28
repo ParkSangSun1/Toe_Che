abc8075200029646bb5c3faad046b552ee7848f9
ParkSangSun1 <appleasz1028102@gmail.com>
2021-05-28 10:52:08 +0900

    [CREATE] SplashActivity Basic Frame

---
+++ app/src/main/java/com/children/toyexchange/UI/Splash/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.children.toyexchange.UI.Splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.children.toyexchange.R
+import com.children.toyexchange.UI.Tutorial.TutorialActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(
+            {
+
+                startActivity(
+                    Intent(
+                        this@SplashActivity,
+                        TutorialActivity::class.java
+                    )
+                )
+                finish()
+            },
+            2000
+        )
+    }
+}
---
+++ app/src/main/java/com/children/toyexchange/UI/Tutorial/TutorialActivity.kt
@@ -0,0 +1,12 @@
+package com.children.toyexchange.UI.Tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.children.toyexchange.R
+
+class TutorialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}
