74e4a037dbdaeb49069845fac6d13c320bebca41
ParkSangSun1 <appleasz1028102@gmail.com>
2021-06-07 20:35:32 +0900

    [CREATE] Bottom Navigation

---
+++ app/src/main/java/com/children/toyexchange/UI/SignUp/SignInActivity.kt
@@ -0,0 +1,14 @@
+package com.children.toyexchange.UI.SignUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.children.toyexchange.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+
+    }
+}
--- app/src/main/java/com/children/toyexchange/UI/Tutorial/TutorialActivity.kt
+++ app/src/main/java/com/children/toyexchange/UI/Tutorial/TutorialActivity.kt
@@ -1,12 +1,23 @@
 package com.children.toyexchange.UI.Tutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.children.toyexchange.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.children.toyexchange.UI.Tutorial.Adapter.TutorialViewPagerAdapter
+import com.children.toyexchange.databinding.ActivityTutorialBinding
 
 class TutorialActivity : AppCompatActivity() {
+
+
+
+
+    private val bgColors = android.R.color.white
+    private lateinit var binding: ActivityTutorialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tutorial)
+        binding = ActivityTutorialBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.pager.adapter = TutorialViewPagerAdapter(bgColors)
+        binding.pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
     }
 }
