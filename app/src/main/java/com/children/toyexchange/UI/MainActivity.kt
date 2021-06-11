2759429ffd113598545e4475fb2e3137eb59ec1e
ParkSangSun1 <apple1028102@gmail.com>
2021-06-11 22:00:20 +0900

    [REFACTORING] MainActivity.kt

--- app/src/main/java/com/children/toyexchange/UI/MainActivity.kt
+++ app/src/main/java/com/children/toyexchange/UI/MainActivity.kt
@@ -3,15 +3,13 @@ package com.children.toyexchange.UI
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.GestureDetector
-import android.view.MotionEvent
 import android.view.View
 import android.view.animation.TranslateAnimation
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.children.toyexchange.UI.Adapter.CategoryAdapter
 import com.children.toyexchange.UI.Adapter.CustomizedRecommendAdapter
 import com.children.toyexchange.UI.Adapter.JustCameUpAdapter
@@ -21,10 +19,8 @@ import com.children.toyexchange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val TAG = "로그"
     lateinit var binding: ActivityMainBinding
     private var upDownWait: Long = 0
-
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,62 +28,55 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
 
-        //파란색 음명 표시 삭제
-        binding.mainScrollview.overScrollMode = View.OVER_SCROLL_NEVER
-        binding.customizedRecommendRecyclerview.overScrollMode = View.OVER_SCROLL_NEVER
-        binding.categoryRecyclerview.overScrollMode = View.OVER_SCROLL_NEVER
-        binding.justCameUpRecyclerview.overScrollMode = View.OVER_SCROLL_NEVER
+        //파란색 음영 표시 삭제
+        shadowDelete(binding.mainScrollview, null)
+        shadowDelete(null, binding.customizedRecommendRecyclerview)
+        shadowDelete(null, binding.categoryRecyclerview)
+        shadowDelete(null, binding.justCameUpRecyclerview)
 
 
         //맞춤 추천 장난감
-        binding.customizedRecommendRecyclerview.layoutManager =
-            LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewManager(binding.customizedRecommendRecyclerview)
         binding.customizedRecommendRecyclerview.adapter = CustomizedRecommendAdapter()
 
 
         //카테고리
-        binding.categoryRecyclerview.layoutManager =
-            LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+        recyclerViewManager(binding.categoryRecyclerview)
         binding.categoryRecyclerview.adapter = CategoryAdapter()
-        setContentView(binding.root)
+
+
+        //방금 올라온 장난감
+        recyclerViewManager(binding.justCameUpRecyclerview)
+        binding.justCameUpRecyclerview.adapter = JustCameUpAdapter()
+
 
         //스크롤 했을때
         binding.mainScrollview.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            Log.i(TAG, "scrolly : $scrollY");
-
             if (System.currentTimeMillis() - upDownWait >= 200) {
                 upDownWait = System.currentTimeMillis()
 
-                Log.i(TAG, "드왔다2");
-
                 //스크롤이 가장 위
                 if (scrollY <= 10) {
-                    Log.i(TAG, "TOP SCROLL");
                     val anim =
                         TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
                     anim.duration = 100
-
                     binding.toyUploadBtn.animation = anim
                     binding.toyUploadBtn.visibility = View.VISIBLE
                 }
 
                 //스크롤 아래로
                 if (scrollY > oldScrollY) {
-                    Log.i(TAG, "Scroll DOWN");
-
                     val anim = TranslateAnimation(0f, binding.toyUploadBtn.width.toFloat(), 0f, 0f)
                     anim.duration = 100
                     binding.toyUploadBtn.animation = anim
                     binding.toyUploadBtn.visibility = View.GONE
-
                 }
+
                 // 스크롤 위로
                 if (scrollY + 5 < oldScrollY) {
-                    Log.i(TAG, "Scroll UP");
                     val anim =
                         TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
                     anim.duration = 100
-
                     binding.toyUploadBtn.animation = anim
                     binding.toyUploadBtn.visibility = View.VISIBLE
                 }
@@ -95,25 +84,34 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        //방금 올라온 장난감
-        binding.justCameUpRecyclerview.layoutManager =
-            LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
-        binding.justCameUpRecyclerview.adapter = JustCameUpAdapter()
-
         binding.toyUploadBtn.setOnClickListener {
-            val intent = Intent(this, ToyUploadActivity::class.java)
-            startActivity(intent)
+            goToyUploadActivity()
         }
+
 
         binding.toyUploadIconBtn.setOnClickListener {
-            val intent = Intent(this, ToyUploadActivity::class.java)
-            startActivity(intent)
+            goToyUploadActivity()
         }
-
 
 
         setContentView(binding.root)
+    }
 
+    private fun goToyUploadActivity() {
+        val intent = Intent(this, ToyUploadActivity::class.java)
+        startActivity(intent)
+    }
+
+    //리사이클러뷰
+    private fun recyclerViewManager(v: RecyclerView) {
+        v.layoutManager =
+            LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+    }
+
+    //스크롤뷰, 리사이클러뷰 음영 삭제
+    private fun shadowDelete(sV: NestedScrollView?, rV: RecyclerView?) {
+        sV?.overScrollMode = View.OVER_SCROLL_NEVER
+        rV?.overScrollMode = View.OVER_SCROLL_NEVER
     }
 
 }
