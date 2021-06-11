aaf2a892310c3e29c9cf27895c3ccbb10a7313ea
ParkSangSun1 <apple1028102@gmail.com>
2021-06-11 22:08:28 +0900

    [REFACTORING] MainActivity.kt

--- app/src/main/java/com/children/toyexchange/UI/MainActivity.kt
+++ app/src/main/java/com/children/toyexchange/UI/MainActivity.kt
@@ -52,35 +52,7 @@ class MainActivity : AppCompatActivity() {
 
         //스크롤 했을때
         binding.mainScrollview.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            if (System.currentTimeMillis() - upDownWait >= 200) {
-                upDownWait = System.currentTimeMillis()
-
-                //스크롤이 가장 위
-                if (scrollY <= 10) {
-                    val anim =
-                        TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
-                    anim.duration = 100
-                    binding.toyUploadBtn.animation = anim
-                    binding.toyUploadBtn.visibility = View.VISIBLE
-                }
-
-                //스크롤 아래로
-                if (scrollY > oldScrollY) {
-                    val anim = TranslateAnimation(0f, binding.toyUploadBtn.width.toFloat(), 0f, 0f)
-                    anim.duration = 100
-                    binding.toyUploadBtn.animation = anim
-                    binding.toyUploadBtn.visibility = View.GONE
-                }
-
-                // 스크롤 위로
-                if (scrollY + 5 < oldScrollY) {
-                    val anim =
-                        TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
-                    anim.duration = 100
-                    binding.toyUploadBtn.animation = anim
-                    binding.toyUploadBtn.visibility = View.VISIBLE
-                }
-            }
+            scrollEvent( v, scrollX, scrollY, oldScrollX, oldScrollY)
         }
 
 
@@ -112,6 +84,39 @@ class MainActivity : AppCompatActivity() {
     private fun shadowDelete(sV: NestedScrollView?, rV: RecyclerView?) {
         sV?.overScrollMode = View.OVER_SCROLL_NEVER
         rV?.overScrollMode = View.OVER_SCROLL_NEVER
+    }
+
+    //스크롤 이벤트
+    private fun scrollEvent(v: View, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int) {
+        if (System.currentTimeMillis() - upDownWait >= 200) {
+            upDownWait = System.currentTimeMillis()
+
+            //스크롤이 가장 위
+            if (scrollY <= 10) {
+                val anim =
+                    TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
+                anim.duration = 100
+                binding.toyUploadBtn.animation = anim
+                binding.toyUploadBtn.visibility = View.VISIBLE
+            }
+
+            //스크롤 아래로
+            if (scrollY > oldScrollY) {
+                val anim = TranslateAnimation(0f, binding.toyUploadBtn.width.toFloat(), 0f, 0f)
+                anim.duration = 100
+                binding.toyUploadBtn.animation = anim
+                binding.toyUploadBtn.visibility = View.GONE
+            }
+
+            // 스크롤 위로
+            if (scrollY + 5 < oldScrollY) {
+                val anim =
+                    TranslateAnimation(binding.toyUploadBtn.width.toFloat() + 1000, 0f, 0f, 0f)
+                anim.duration = 100
+                binding.toyUploadBtn.animation = anim
+                binding.toyUploadBtn.visibility = View.VISIBLE
+            }
+        }
     }
 
 }
