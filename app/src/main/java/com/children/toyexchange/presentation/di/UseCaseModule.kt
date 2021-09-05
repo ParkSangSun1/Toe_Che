da5502f18fe0e0c4ca2da4447f6f8e0af34311d8
ParkSangSun <appleasz1028102@gmail.com>
2021-09-05 12:59:58 +0900

    [ADD] UseCaseModule 추가

--- app/src/main/java/com/children/toyexchange/presentation/di/UseCaseModule.kt
+++ app/src/main/java/com/children/toyexchange/presentation/di/UseCaseModule.kt
@@ -1,4 +1,33 @@
 package com.children.toyexchange.presentation.di
 
+import com.children.toyexchange.domain.repository.FirebaseRepository
+import com.children.toyexchange.domain.usecase.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class UseCaseModule {
+    @Provides
+    @Singleton
+    fun provideCallUserInfoUseCase(repository: FirebaseRepository) = CallUserInfoUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideCheckUserNickNameUseCase(repository: FirebaseRepository) = CheckUserNickNameUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideSaveUserInfoUseCase(repository: FirebaseRepository) = SaveUserInfoUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideSaveUserNickNameUseCase(repository: FirebaseRepository) = SaveUserNickNameUseCase(repository)
+
+    @Provides
+    @Singleton
+    fun provideSaveUserProfileUseCase(repository: FirebaseRepository) = SaveUserProfileUseCase(repository)
 }
