950e8eb519a8819eba4e53de3e266d6d12b72435
ParkSangSun <appleasz1028102@gmail.com>
2021-09-05 13:15:44 +0900

    [UPDATE] Url숨기기

--- app/src/main/java/com/children/toyexchange/data/repository/datasource/RemoteFirebaseDataSource.kt
+++ app/src/main/java/com/children/toyexchange/data/repository/datasource/RemoteFirebaseDataSource.kt
@@ -2,6 +2,7 @@ package com.children.toyexchange.data.repository.datasource
 
 import android.net.Uri
 import com.children.toyexchange.data.models.UserSignIn
+import com.children.toyexchange.presentation.widget.utils.FirebaseUrl.FirebaseStorageUrl
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -12,10 +13,9 @@ class RemoteFirebaseDataSource @Inject constructor(
     private val firebaseRtdb: FirebaseDatabase,
     private val firebaseStorage: FirebaseStorage
     ) {
-
     fun checkUserNickName(nickName : String) = firebaseRtdb.reference.child("userNickName").child(nickName)
 
-    fun saveUserProfile(profile : Uri, fileName : String) = firebaseStorage.getReferenceFromUrl("gs://toyexchange-90199.appspot.com").child("images/$fileName").putFile(profile)
+    fun saveUserProfile(profile : Uri, fileName : String) = firebaseStorage.getReferenceFromUrl(FirebaseStorageUrl).child("images/$fileName").putFile(profile)
 
     fun callUserInfo() = firebaseRtdb.reference.child("userAccountInfo")
 
---
+++ app/src/main/java/com/children/toyexchange/presentation/widget/utils/FirebaseUrl.kt
@@ -0,0 +1,6 @@
+package com.children.toyexchange.presentation.widget.utils
+
+
+object FirebaseUrl {
+    val FirebaseStorageUrl = "gs://toyexchange-90199.appspot.com"
+}
