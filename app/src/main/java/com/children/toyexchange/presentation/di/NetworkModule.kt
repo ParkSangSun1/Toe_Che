4898f6d3aeba409d0dd1f6eebcfbd05593d64b4b
ParkSangSun <appleasz1028102@gmail.com>
2021-10-03 17:08:52 +0900

    [ADD] Api module

---
+++ app/src/main/java/com/children/toyexchange/presentation/di/NetworkModule.kt
@@ -0,0 +1,67 @@
+package com.children.toyexchange.presentation.di
+
+import com.children.toyexchange.data.api.KakaoAddressApi
+import com.children.toyexchange.presentation.widget.utils.ApiUrl.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+// Singleton, Provides 쓸때 private 말고 public 으로 해야한다.
+object NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(getLoggingInterceptor())
+            .build()
+
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            //json 변화기 Factory
+            .client(provideHttpClient())
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideKakaoAddressApiService(retrofit: Retrofit): KakaoAddressApi {
+        return retrofit.create(KakaoAddressApi::class.java)
+    }
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+
+}
