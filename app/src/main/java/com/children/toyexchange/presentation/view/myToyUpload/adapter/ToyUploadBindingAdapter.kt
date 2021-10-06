20358d807d7decf64f9520f6302bc3bd31bdec76
ParkSangSun <appleasz1028102@gmail.com>
2021-10-06 14:27:40 +0900

    [UPDATE] 주소 api response 공백값 처리

--- app/src/main/java/com/children/toyexchange/presentation/view/myToyUpload/adapter/ToyUploadBindingAdapter.kt
+++ app/src/main/java/com/children/toyexchange/presentation/view/myToyUpload/adapter/ToyUploadBindingAdapter.kt
@@ -11,18 +11,22 @@ object ToyUploadBindingAdapter {
     @JvmStatic
     @BindingAdapter("zone_no")
     fun setZoneNo(text: TextView, zoneNo: String?) {
-        text.text = zoneNo
+        Log.d("로그","널인듯? $zoneNo")
+        if (zoneNo == null || zoneNo =="") text.text = "해당하는 값이 없습니다"
+        else text.text = zoneNo
     }
 
     @JvmStatic
     @BindingAdapter("road_address_name")
     fun setRoadAddressName(text: TextView, addressName: String?) {
-        text.text = addressName
+        if (addressName == null) text.text = "해당하는 값이 없습니다"
+        else text.text = addressName
     }
 
     @JvmStatic
     @BindingAdapter("address_name")
     fun setAddressName(text: TextView, addressName: String?) {
-        text.text = addressName
+        if (addressName == null) text.text = "해당하는 값이 없습니다"
+        else text.text = addressName
     }
 }
