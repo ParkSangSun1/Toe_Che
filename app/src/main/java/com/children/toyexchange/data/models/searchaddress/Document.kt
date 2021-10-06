fe1ed64af8875dff25a5057c5a9445198d68dde9
ParkSangSun <appleasz1028102@gmail.com>
2021-10-06 20:37:10 +0900

    [UPDATE] null

--- app/src/main/java/com/children/toyexchange/data/models/searchaddress/Document.kt
+++ app/src/main/java/com/children/toyexchange/data/models/searchaddress/Document.kt
@@ -1,10 +1,10 @@
 package com.children.toyexchange.data.models.searchaddress
 
 data class Document(
-    val address: AddressItem,
-    val address_name: String,
-    val address_type: String,
-    val road_address: RoadAddress,
-    val x: String,
-    val y: String
+    val address: AddressItem?,
+    val address_name: String?,
+    val address_type: String?,
+    val road_address: RoadAddress?,
+    val x: String?,
+    val y: String?
 )
--- app/src/main/java/com/children/toyexchange/data/models/searchaddress/RoadAddress.kt
+++ app/src/main/java/com/children/toyexchange/data/models/searchaddress/RoadAddress.kt
@@ -1,6 +1,9 @@
 package com.children.toyexchange.data.models.searchaddress
 
 data class RoadAddress(
-    val address_name : String,
-    val zone_no : String
+    val address_name : String?,
+    val zone_no : String?,
+    val region_1depth_name : String?,
+    val region_2depth_name : String?,
+    val region_3depth_name : String?
 )
