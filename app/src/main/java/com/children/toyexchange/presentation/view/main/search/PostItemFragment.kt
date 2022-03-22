3f0cdb897e37405fa5ac17fbe297dd212fd07eeb
ParkSangSun <appleasz1028102@gmail.com>
2022-03-23 08:27:36 +0900

    [CREATE] PostItemFragment

---
+++ app/src/main/java/com/children/toyexchange/presentation/view/main/search/PostItemFragment.kt
@@ -0,0 +1,12 @@
+package com.children.toyexchange.presentation.view.main.search
+
+import com.children.toyexchange.R
+import com.children.toyexchange.databinding.FragmentPostItemBinding
+import com.children.toyexchange.presentation.base.BaseFragment
+
+class PostItemFragment : BaseFragment<FragmentPostItemBinding>(R.layout.fragment_post_item) {
+
+    override fun init() {
+
+    }
+}
